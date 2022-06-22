fd1fa9d0f00f3f02737b833be2657674a07bead4
Kanha Tiwari <tiwarikanha26@gmail.com>
2022-06-22 10:36:59 +0530

    User Setting Component has been compeleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import { HomePage } from "./Components/Pages/HomePage/HomePage";
 import { PostView } from "./Components/Pages/PostView/PostView";
 import { UploadPost } from "./Components/Pages/UploadPost/UploadPost";
+import { UserSettting } from "./Components/Pages/UserSetting/UserSetting";
 import TopBar from "./Components/TopBar/TopBar";
 
 function App() {
   return (
     <>
       <TopBar />
-      <UploadPost />
+      {/* <UploadPost /> */}
       {/* <PostView /> */}
       {/* <HomePage /> */}
+      <UserSettting />
     </>
   );
 }
---
+++ frontend/src/Components/Pages/UserSetting/UserSetting.jsx
@@ -0,0 +1,60 @@
+import "./UserSetting.css";
+import myimage from "../../../Images/User.jpeg";
+import { SideBar } from "../../SideBar/SideBar";
+
+export const UserSettting = () => {
+  return (
+    <div className="setting">
+      <div className="settingcontainer">
+        <div className="settingWrapper">
+          <img src={myimage} alt="" className="settingimg" />
+          <label htmlFor="changeimg">
+            <i class=" changeimgicon fileinputIcon fa-solid fa-circle-plus"></i>
+          </label>
+          <input type="file" id="changeimg" className="changeimg" />
+        </div>
+        <div className="inputcontainer">
+          {/* Basic Info  */}
+          <div className="basicinfo">
+            <div className="name">
+              <label>
+                Change Your Name :<br />{" "}
+                <input type="text" className="changename" />
+              </label>
+            </div>
+            <div className="email">
+              <label>
+                Change Your Email :<br></br>{" "}
+                <input type="text" className="changeemail" />
+              </label>
+            </div>
+          </div>
+
+          <br />
+          {/* Desctiption Area */}
+          <label style={{ "text-align": "center", margin: "10px" }}>
+            Change Profile Description :
+          </label>
+          <textarea
+            placeholder="Change Your discription"
+            className="textareainput"
+          ></textarea>
+
+          {/* Password Area */}
+          <div className="passwordarea">
+            <label>
+              Change Your Password :<br />
+              <input type="password" className="changepassword" />
+            </label>
+
+            <label>
+              Enter Current Password :<br />
+              <input type="password" className="currentpassword" />
+            </label>
+          </div>
+        </div>
+      </div>
+      <SideBar />
+    </div>
+  );
+};
