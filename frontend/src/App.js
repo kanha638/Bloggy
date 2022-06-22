0413c8626fd9b831cfe5e8bf3179eba1c965c9c9
Kanha Tiwari <tiwarikanha26@gmail.com>
2022-06-22 16:48:09 +0530

    added react router-dom link and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
 import { HomePage } from "./Components/Pages/HomePage/HomePage";
 import { PostView } from "./Components/Pages/PostView/PostView";
 import { UploadPost } from "./Components/Pages/UploadPost/UploadPost";
-import { UserSettting } from "./Components/Pages/UserSetting/UserSetting";
+import { UserSetting } from "./Components/Pages/UserSetting/UserSetting";
 import TopBar from "./Components/TopBar/TopBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <TopBar />
-      {/* <UploadPost /> */}
-      {/* <PostView /> */}
-      {/* <HomePage /> */}
-      <UserSettting />
+      <BrowserRouter>
+        <TopBar />
+        {/* <UploadPost /> */}
+        {/* <PostView /> */}
+        {/* <HomePage /> */}
+        {/* <UserSettting /> */}
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/write" element={<UploadPost />}></Route>
+          <Route path="/settings" element={<UserSetting />}></Route>
+          <Route path="/post/:id" element={<PostView />}></Route>
+          <Route path="/resister" element={"Resister"}></Route>
+          <Route path="/Login" element={"Login"}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/Components/Pages/HomePage/HomePage.jsx
+++ frontend/src/Components/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Blogs } from '../../Blogs/Blogs';
-import { ImageHead } from '../../ImageHead/ImageHead';
-import { SideBar } from '../../SideBar/SideBar';
+import React from "react";
+import { Blogs } from "../../Blogs/Blogs";
+import { ImageHead } from "../../ImageHead/ImageHead";
+import { SideBar } from "../../SideBar/SideBar";
 import "./HomePage.css";
 
 export const HomePage = () => {
-  return (<>
-    <ImageHead />
-    <div className="content">
-      <Blogs />
-      <SideBar/>
-    </div>
+  return (
+    <>
+      <ImageHead />
+      <div className="content">
+        <Blogs />
+        <SideBar />
+      </div>
     </>
-  )
-}
+  );
+};
