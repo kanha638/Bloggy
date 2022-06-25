f2f120621a319220658198df53ad829d3a1940d8
Hyper-ActiveX <jayactive11112@gmail.com>
2022-06-25 09:29:03 +0530

    Created About Page

---
+++ frontend/src/Components/Pages/About/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./About.css";
+
+export const About = () => {
+  return (
+    <>
+      <div className="aboutpage">
+      <div className="head">About US</div>
+      <div className="para">This Website is Publised on 6th july 2022 
+      .________ Contributers is Jay Kumar Pandit And Kanha Tiwari,
+       both work in frontend as well as backend
+      .Our ________ contributors work hard, and have 
+      dedicated countless hours to build a tool that democratizes 
+      publishing.________ is open source
+       software that is both free and priceless.</div>
+        <div className="head">
+            Our Mission
+        </div>
+        <div className="para">
+        ________ is software designed for resume and to create new ideas and ease to use.
+          We believe great software should work with minimum set 
+          up, so you can focus on sharing your story, product, or 
+          services freely. The basic ________ software is simple 
+          and predictable so you can easily get started. It also offers
+         powerful features for growth and success.
+        </div>
+        <div className="para">
+        Supporting this idea by Jay kumar Pandit and Kanha Tiwari 
+        collaborating on and contributing to this project.
+         The ________ community is welcoming and inclusive.
+          Our contributors’ passion drives the success
+           of ________ which,
+         in turn, helps you reach your goals.
+        </div>
+
+        <div className="head">
+        Our Story
+        </div>
+        <div className="para">
+        ________ started in June 2022 by Kanha and Jay many difficulties come but we work 
+        hard day by day .The need for an elegant, well-architected
+         personal publishing system was clear even then.________ is Fully MERN Stack
+         we use react for frontend and nodejs for backend and for Database we used
+         MongoDB.
+        </div>
+
+        <div className="para">
+        The ________ open source project has evolved in progressive 
+        ways over time. ________ provides 
+         the opportunity for anyone to create and share post.
+          People with a limited tech experience can use it “out of the box”, and 
+        more tech-savvy folks can customize it in remarkable ways.
+        </div>
+
+        <div className="head">
+        Bill of Rights
+        </div>
+        <div className="para">
+        ________ is licensed under the ___________________ which provide free 
+        licence.Consider this the ________ Bill of Rights:
+        </div>
+
+      </div>
+    </>
+  );
+};
