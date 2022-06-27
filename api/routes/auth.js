d277b47d01b8ac9d9cfe6e10d36cef7ddcc2584a
Kanha Tiwari <tiwarikanha26@gmail.com>
2022-06-27 16:06:13 +0530

    added password encryption method to the user resistration

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,17 +1,24 @@
 const router = require("express").Router();
 // const { response } = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // User Resistration
 
 router.post("/register", async (req, resp) => {
   try {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const user = new User({ name, email, password });
-    const newuser = await user.save();
-    return resp.status(200).json(newuser);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+
+    return resp.status(200).json(result);
   } catch (err) {
     return resp.status(505).json(err);
   }
